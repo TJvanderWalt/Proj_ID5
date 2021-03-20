06a29caced22f91bb6f65c8bf1c417755c28a40b
Outoppie TJ <vanderwalttjaartj@gmail.com>
2021-03-20 12:19:51 +0200

    adding flask converters

--- app.py
+++ app.py
@@ -13,7 +13,23 @@ def hello_world():
 # dynamic route
 @app.route("/test/<search_query>")
 def search(search_query):
-return search_query
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
 # start the flask development server using the run() method
 if __name__ == "__main__":
     app.run()
